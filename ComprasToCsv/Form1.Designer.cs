6d56724670f5d907df863bc84fc826a75c0c4283
Kildare Lauser <kildarealauser@gmail.com>
2020-05-01 09:10:19 -0400

    first commit

---
+++ ComprasToCsv/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ComprasToCsv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.from = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.to = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GENERAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // from
+            // 
+            this.from.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.from.Location = new System.Drawing.Point(12, 29);
+            this.from.Name = "from";
+            this.from.Size = new System.Drawing.Size(98, 20);
+            this.from.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Desde:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Hasta";
+            // 
+            // to
+            // 
+            this.to.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.to.Location = new System.Drawing.Point(12, 82);
+            this.to.Name = "to";
+            this.to.Size = new System.Drawing.Size(98, 20);
+            this.to.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 123);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.to);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.from);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "LEONUX COMPRAS A CSV";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker from;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker to;
+    }
+}
+
---
+++ ComprasToCsv/Form1.cs
@@ -0,0 +1,104 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComprasToCsv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            string fromDate = from.Value.ToString("yyyy-MM-dd");
+            string toDate   = to.Value.ToString("yyyy-MM-dd");
+
+            //var connectionString = @"Data Source=10.10.0.250\GRUPOPASEO,1433;Network Library=DBMSSOCN;Initial Catalog=GRUPOPASEO78LQNVEN;User ID=sa;Password=123;MultipleActiveResultSets=True";
+            //var selectQuery = @"SELECT  `razon_social` AS PROVEEDOR,  `fecha_registro` AS `FECHA REGISTRO`,  `fecha` AS  `FECHA DOCUMENTO` ,  `documento` AS DOCUMENTO, total AS `MONTO FACTURA`,  `impuesto` AS  `MONTO IVA`,  `tasa_retencion_iva` AS `% RET IVA`, `retencion_iva` AS `IVA RETENIDO`,  `tasa_retencion_islr` AS `% RET ISLR`,  `retencion_islr` AS `ISLR RETENIDO`, base + ( impuesto - retencion_iva ) AS TOTAL FROM  `compras`  WHERE fecha >= '2019-01-01' AND fecha <= '2019-01-03'";
+            var selectQuery = "SELECT razon_social, fecha_registro, fecha, documento, total as MONTO, retencion_iva, tasa_retencion_iva, retencion_islr, tasa_retencion_islr, base + exento + ( impuesto - retencion_iva ) AS TOTAL FROM compras WHERE fecha_registro >= '" + fromDate+ "' AND fecha_registro <= '" + toDate+"'";
+            var server = "10.10.0.199";
+            var database = "00000002";
+            var username = "root";
+            var password = "123";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + username + ";" + "PASSWORD=" + password + ";";
+
+            try {
+                var table = ReadTable(connectionString, selectQuery);
+
+                //WriteToFile(table, @"C:\temp\outputfile.csv", false, ",");
+
+                StringBuilder sb = new StringBuilder();
+
+                IEnumerable<string> columnNames = table.Columns.Cast<DataColumn>().
+                                                  Select(column => column.ColumnName);
+                sb.AppendLine(string.Join(",", columnNames));
+
+                foreach (DataRow row in table.Rows)
+                {
+                    IEnumerable<string> fields = row.ItemArray.Select(field =>
+                      string.Concat("\"", field.ToString().Replace("\"", "\"\""), "\""));
+                    sb.AppendLine(string.Join(",", fields));
+                }
+                var time = DateTime.Now;
+                string formattedTime = time.ToString("yyyyMMddhhmmss");
+                File.WriteAllText(@"C:\temp\compras-" + formattedTime + ".csv", sb.ToString());
+                MessageBox.Show(@"ARCHIVO GUARDADO EN C:\temp\compras-" + formattedTime + ".csv", "MENSAJE", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            } catch (Exception _) {
+                MessageBox.Show("ERROR AL GENERAR EL ARCHIVO", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            button1.Enabled = true;
+        }
+
+        public static DataTable ReadTable(string connectionString, string selectQuery)
+        {
+            var returnValue = new DataTable();
+
+            var conn = new MySqlConnection(connectionString);
+
+            try
+            {
+                conn.Open();
+                var command = new MySqlCommand(selectQuery, conn);
+
+                using (var adapter = new MySqlDataAdapter(command))
+                {
+                    adapter.Fill(returnValue);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                    conn.Close();
+            }
+
+            return returnValue;
+        }        
+    }
+}
